f89f8f8dc4f6ec42909b0879dcf8868da3caf8b6
Cameron <camlecuyer@yahoo.com>
2018-04-14 15:52:29 -0500

    Added field checking to register

--- DriftersConnect/src/pages/register/register.ts
+++ DriftersConnect/src/pages/register/register.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {AlertController, NavController} from 'ionic-angular';
 import {AngularFireAuth, AngularFireAuthModule} from "angularfire2/auth";
-import {HomePage} from "../home/home";
+import {LoginPage} from "../login/login";
 
 
 @Component({
@@ -12,32 +12,50 @@ export class RegisterPage {
 
   username = "";
   psw ="";
+  fname = "";
+  lname = "";
+  address = "";
+  zip = "";
+  pswConfirm = "";
 
   constructor(private  alertCtrl:AlertController,private fire: AngularFireAuth, public navCtrl: NavController) {
 
   }
 
-  alert(message: String){
+  alert(message: string){
     this.alertCtrl.create({
       title:'Info!',
-      subTitle:'message',
+      subTitle:message,
       buttons: ['OK']
     }).present();
   }
 
   registerUser(){
-    this.fire.auth.createUserWithEmailAndPassword(this.username.valueOf(), this.psw.valueOf()).then(data =>{
-      console.log("got data from Firebase : ", data);
-      this.alert("You are logged in !")
-      this.navCtrl.push(HomePage);
+    if(this.username.valueOf() != "" && this.psw.valueOf() != "" && this.fname.valueOf() != "" && this.lname.valueOf() != ""
+      && this.address.valueOf() != "" && this.zip.valueOf() != "" && this.pswConfirm.valueOf() != "")
+    {
+      if(this.pswConfirm.valueOf() == this.psw.valueOf())
+      {
+        this.fire.auth.createUserWithEmailAndPassword(this.username.valueOf(), this.psw.valueOf()).then(data =>{
+          console.log("Got data from Firebase: ", data);
+          this.alert("You are logged in!")
+          this.navCtrl.setRoot(LoginPage);
 
-    }).catch(error =>{
-      console.log("error in registeration : ", error);
+        }).catch(error =>{
+          this.alert(error);
+          console.log("error in registration : ", error);
+        });
+      }
+      else
+      {
+        this.alert("Passwords do not match");
+      }
+    }
+    else
+    {
+      this.alert("Please fill out all fields")
+    }
 
-    });
     console.log("will register using credentials : username is "+ this.username +" and psw is "+this.psw )
-
-
-}
-
+  }
 }
